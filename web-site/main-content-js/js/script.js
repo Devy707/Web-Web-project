df1bb84c45529d5b95c045e1d4be4ac798373aa8
Bogdan <stetsenko.bogdan.work@gmail.com>
2021-10-05 21:26:35 +0300

    add lab

---
+++ web-site/main-content-js/js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+   
+    breakpoints: {
+        320: {
+            slidesPerView:1
+        },
+        994: {
+            slidesPerView:2
+        },
+        
+        1788:{
+            slidesPerView:4
+        }
+    },
+    loop:true,
+  });
---
+++ web-site/main-js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+   
+   breakpoints: {
+       320: {
+           slidesPerView:1
+       },
+       994: {
+           slidesPerView:2
+       },
+       
+       1788:{
+           slidesPerView:4
+       }
+   },
+   loop:true,
+ });
+
+ 
